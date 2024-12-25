73fe073e9c1a840e97af298899a4b8982cc789a2
parashsubedi27@gmail.com <parashsubedi37@gmail.com>
2024-12-25 09:16:45 +0545

    added a login page

--- level13.js
+++ level13.js
@@ -2,7 +2,8 @@ const name = 'ram thapa'
 
 //Q1. Expected output:
 // ['thapa','ram']
-
+const reversedName = name.split(" ").reverse().join(" ");
+console.log(reversedName);
 
 
 //Q2. Expected output
