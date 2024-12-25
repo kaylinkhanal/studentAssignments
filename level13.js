d91c5b92edf1db90375bb74ff0b17a8dd645a2ac
Samirghimire-5 <samirghimire9841@gmail.com>
2024-12-25 13:43:43 +0545

    solved Q 1-3

--- level13.js
+++ level13.js
@@ -3,7 +3,7 @@ const name = 'ram thapa'
 //Q1. Expected output:
 // ['thapa','ram']
 
-console.log(name.split(' ').reverse())
+console.log(name.split(' ').reverse());
 
 
 
@@ -12,8 +12,20 @@ console.log(name.split(' ').reverse())
 // 'ram': 'thapa'
 // }
 
+let arr = name.split(' ');
+let obj = Object.fromEntries([arr]);
+console.log(obj);
+
+
 
 //Q3. expected output: 
 // {
     // username: 'Ram Thapa'
 // },
+
+
+const object = {
+    username: name
+}
+console.log(object)
+
