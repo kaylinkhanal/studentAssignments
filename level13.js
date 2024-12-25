543894cb42d033d4dc9349b4335b649386d148c6
Ajay Gurung <gooronz.ajay@gmail.com>
2024-12-25 09:20:43 +0545

    level 13 assignment done

--- level13.js
+++ level13.js
@@ -2,16 +2,23 @@ const name = 'ram thapa'
 
 //Q1. Expected output:
 // ['thapa','ram']
-
+x = name.split(' ').reverse()
+console.log(x)
 
 
 //Q2. Expected output
 //  {
 // 'ram': 'thapa'
 // }
+name_copy = {}
+name_copy[x[1]] = x[0]
+console.log(name_copy)
 
 
 //Q3. expected output: 
 // {
     // username: 'Ram Thapa'
 // },
+name_copy2 = {}
+name_copy2['username'] = name
+console.log(name_copy2)
