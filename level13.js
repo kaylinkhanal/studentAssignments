767e62134726faa46998505b2e5df437fa7f3a58
[firstname lastname] <[dhamib@gmail.com]>
2024-12-25 12:18:48 +0545

    Add file level13.js

--- level13.js
+++ level13.js
@@ -2,16 +2,22 @@ const name = 'ram thapa'
 
 //Q1. Expected output:
 // ['thapa','ram']
+console.log(name.split(' ').reverse())
 
 
 
 //Q2. Expected output
 //  {
 // 'ram': 'thapa'
+const [fname, lname] = name.split(' ');
+const result = { [fname]: lname }; // Use dynamic key to create the object
+
+console.log(result);
 // }
 
 
 //Q3. expected output: 
 // {
     // username: 'Ram Thapa'
+    console.log({username:'Ram Thapa'})
 // },
