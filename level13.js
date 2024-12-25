c0b97c99314ae101d69b4e2c49aee8ff181a98f3
sushimchaudhary <sushimchaudhary1@gmail.com>
2024-12-25 19:53:19 +0545

    level13 solve the Question

--- level13.js
+++ level13.js
@@ -2,7 +2,7 @@ const name = 'ram thapa'
 
 //Q1. Expected output:
 // ['thapa','ram']
-console.log(name.split())
+console.log(name.split(' ').reverse());
 
 
 
@@ -11,8 +11,29 @@ console.log(name.split())
 // 'ram': 'thapa'
 // }
 
+const parts = name.split(' ');
+const result = { [parts[0]]: parts[1] };
+console.log(result);
 
 //Q3. expected output: 
 // {
     // username: 'Ram Thapa'
 // },
+
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+const [firstName, lastName] = name.split(' ');
+const username = (`${capitalize(firstName)} ${capitalize(lastName)}`);
+console.log({username});
+
+
+
+// function to capitalize
+
+const capitalizeWord = (str) => {
+    return str
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase()+ word.slice(1).toLowerCase())
+    .join(' ');
+};
+const userObject = {username: capitalizeWord(name)};
+console.log(userObject)
