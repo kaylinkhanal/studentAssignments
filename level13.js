57adde661992a5c7d057a71d5a3624ad9313f3b3
=raj-maharjan99 <=rjuallone@gmail.com>
2024-12-25 12:25:01 -0600

    Solved Level 13 questions

--- level13.js
+++ level13.js
@@ -1,18 +1,31 @@
-const name = 'ram thapa'
+const name = "ram thapa";
 
 //Q1. Expected output:
 // ['thapa','ram']
-console.log(name.split())
-
-
+console.log(name.split(" ").reverse());
 
 //Q2. Expected output
 //  {
 // 'ram': 'thapa'
 // }
 
+const [ram, thapa] = name.split(" ");
+const output = { [ram]: thapa };
+console.log(output);
 
-//Q3. expected output: 
+console.log("typeof ram :", typeof ram);
+//Q3. expected output:
 // {
-    // username: 'Ram Thapa'
+// username: 'Ram Thapa'
 // },
+
+// first changed key name
+const updatedKeyName = { username: name };
+
+// changed first index from lower case to upper case
+
+updatedKeyName.username =
+  updatedKeyName.username.charAt(0).toUpperCase() +
+  updatedKeyName.username.slice(1);
+
+console.log(updatedKeyName);
