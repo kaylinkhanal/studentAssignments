9b90d21834112c32b8321ea70f4dd01fee29ed10
roshandangol0506 <roshan.dangol00@gmail.com>
2024-12-25 09:17:53 +0545

    secibd commit

--- level0.js
+++ level0.js
@@ -1,25 +1,23 @@
 // ---Arithematic operator problem
 
 // Q1. get a reminder when 10 is divided by 3 and console.log the output
-console.log(10%3)
+console.log(10 % 3);
 
 //Q2. get the percentage scored if the fullmarks is 200
-var comp = 'ram'
-var gk = 88
-console.log((comp+gk)/200*100)
+var comp = "ram";
+var gk = 88;
+console.log(((comp + gk) / 200) * 100);
 
 //Q3. calculate the area of circle:
-var radius = 98
-console.log(22/7*radius**2)  // Math.PI
+var radius = 98;
+console.log((22 / 7) * radius ** 2); // Math.PI
 
 //Q4. convert -99 to 99
-var num = -99
-console.log(num * -1)  // Math.abs
+var num = -99;
+console.log(num * -1); // Math.abs
 
 //Q5. find cube fo 25
-var temp = 25
-console.log(temp**3)   //Math.pow(temp, 3)
+var temp = 25;
+console.log(temp ** 3); //Math.pow(temp, 3)
 
-
-
-
+console.log("roshan");
--- level13.js
+++ level13.js
@@ -1,17 +1,16 @@
-const name = 'ram thapa'
+const name = "ram thapa";
 
 //Q1. Expected output:
 // ['thapa','ram']
-
-
 
 //Q2. Expected output
 //  {
 // 'ram': 'thapa'
 // }
 
-
-//Q3. expected output: 
+//Q3. expected output:
 // {
-    // username: 'Ram Thapa'
+// username: 'Ram Thapa'
 // },
+
+console.log("roshan");
