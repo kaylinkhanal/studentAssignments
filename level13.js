004457998cb88edc61b07c3dcdf28091a8baaba7
aakash-rauniyar <aakash2003rauniyar@gmail.com>
2024-12-26 15:30:27 +0545

    Task Completed

--- level13.js
+++ level13.js
@@ -2,18 +2,21 @@ const name = 'ram thapa'
 
 //Q1. Expected output:
 // ['thapa','ram']
-const new = name.split("");
-console.log(new);
-
+n = name.split(' ').reverse()
+console.log(n);
 
 
 //Q2. Expected output
 //  {
 // 'ram': 'thapa'
 // }
+let [FN, LN] = name.split(' ');
+let answer = {[FN]: LN};
+console.log(answer);
 
 
 //Q3. expected output: 
 // {
     // username: 'Ram Thapa'
 // },
+console.log(answer(0).touppercase())
