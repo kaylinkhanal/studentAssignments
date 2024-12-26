56f7c3e4ade9fe9536c8080dfbd49383565ffb64
Shiva Shrestha <itsshivashrestha@gmail.com>
2024-12-26 23:24:37 +0545

    assignment submitted

--- level13.js
+++ level13.js
@@ -2,7 +2,7 @@ const name = 'ram thapa'
 
 //Q1. Expected output:
 // ['thapa','ram']
-const output = name.split('').reverse();
+const output = name.split(' ').reverse();
 console.log(output)
 
 
@@ -11,10 +11,16 @@ console.log(output)
 //  {
 // 'ram': 'thapa'
 // }
+const A = name.split(' ');
+const output1 = {[A[0]]: A[1]}
+console.log(output1)
 
 
 //Q3. expected output: 
 // {
     // username: 'Ram Thapa'
 // },
+const output2 = name[0].toUpperCase()+name.slice(1,3)+ ' '+ name[4].toUpperCase()+name.slice(5)
+const output3 = {username:output2}
+console.log(output3)
 
