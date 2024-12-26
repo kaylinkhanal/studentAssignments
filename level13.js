f8c0970772bafe91772e1cb57ab75228edea0220
rahulcoder2 <rahulchaudharycsit077@gmail.com>
2024-12-26 08:25:49 +0545

    add: level13 output

--- level13.js
+++ level13.js
@@ -2,17 +2,34 @@ const name = 'ram thapa'
 
 //Q1. Expected output:
 // ['thapa','ram']
-console.log(name.split())
 
+const output1 = name.split(' ').reverse()
 
+console.log(output1)
 
 //Q2. Expected output
 //  {
 // 'ram': 'thapa'
 // }
 
+const [firstName, lastName] = name.split(' ')
+
+const output2 = {
+    [firstName] : lastName,
+}
+console.log(output2)
 
 //Q3. expected output: 
 // {
-    // username: 'Ram Thapa'
+    // name: 'Ram Thapa'
 // },
+
+const firstNameCapitalize = firstName.toUpperCase().charAt(0) + firstName.slice(1)
+const   lastNameCapitalize = lastName.charAt(0).toUpperCase() + lastName.slice(1)
+const capitalizeFullName = `${firstNameCapitalize} ${lastNameCapitalize}`
+
+const output3 = {
+    username : capitalizeFullName
+}
+
+console.log(output3)
