a497fb09c5ee5ccd5963b02b66237ad1229f437e
ipradeepshah <ipradeepshah@gmail.com>
2024-12-27 14:23:39 +0545

    task of 25 dec

--- level13.js
+++ level13.js
@@ -2,16 +2,28 @@ const name = 'ram thapa'
 
 //Q1. Expected output:
 // ['thapa','ram']
-
-
+console.log (name.split(' ').reverse());
 
 //Q2. Expected output
 //  {
 // 'ram': 'thapa'
 // }
 
+const [firstName, lastName] = name.split(' ');
+const result = {[firstName] : lastName };
+console.log(result);
+
 
 //Q3. expected output: 
 // {
     // username: 'Ram Thapa'
-// },
+// }
+
+//const name = 'ram thapa';
+const formattedName = name
+  .split(' ')
+  .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+  .join(' ');
+
+const result = { username: formattedName };
+console.log(result);
