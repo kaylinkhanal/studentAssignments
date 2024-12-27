3913fab24ada7f59367da221286eb94d4a0c2c34
Dr-vegapunk <Ishankarki7624@gmail.com>
2024-12-27 21:02:58 +0545

    added level 13 assignment

--- level13.js
+++ level13.js
@@ -2,7 +2,7 @@ const name = 'ram thapa'
 
 //Q1. Expected output:
 // ['thapa','ram']
-console.log(name.split())
+console.log(name.split(' ').reverse())
 
 
 
@@ -10,9 +10,15 @@ console.log(name.split())
 //  {
 // 'ram': 'thapa'
 // }
+const [first,last] = name.split(' ')
+console.log({[first]:last})
+
 
 
 //Q3. expected output: 
 // {
     // username: 'Ram Thapa'
 // },
+const first1= first.charAt(0).toUpperCase()+first.slice(1)
+const last1= last.charAt(0).toUpperCase()+last.slice(1)
+console.log({username: first1 + ' ' + last1})
