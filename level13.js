4f961b3a9ca3f228e252d7409b22d743e4b0a2c7
Prem kala <114562215+Premkalagithub@users.noreply.github.com>
2024-12-28 20:03:03 +0545

    Added Task

--- level13.js
+++ level13.js
@@ -9,9 +9,15 @@ console.log(name.split(' '). reverse ())
 //  {
 // 'ram': 'thapa'
 // }
-
+const newname= name.split("")
+console.log(`{'${name[0]}':} '${name[1]}`)
 
 //Q3. expected output: 
 // {
     // username: 'Ram Thapa'
 // },
+const new1={
+    username:name,
+
+}
+console.log(new1)
