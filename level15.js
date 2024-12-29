6451032ee603816010bd5a7bb83cfe30ed1c961c
rahulcoder2 <rahulchaudharycsit077@gmail.com>
2024-12-29 21:35:17 +0545

    add :level15 output

--- level15.js
+++ level15.js
@@ -19,3 +19,20 @@ const subjects= {
 //     },
 //     subjects: [35,36]
 // }
+
+// get data using destructure 
+
+const { user, subjects: [subject1, subject2] } = details;
+
+const output = {
+    user,
+    subjects: [subjects[subject1], subjects[subject2]],
+};
+
+console.log(output);
+
+//  get data without using destructure
+
+// details.subjects[0] = subjects.math
+// details.subjects[1] = subjects.science
+// console.log(details)
