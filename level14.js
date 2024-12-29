a8c0a9c078dc5ce8ef3049010a430485be0b6c30
Samirghimire-5 <samirghimire9841@gmail.com>
2024-12-29 09:13:55 +0545

    solved Q 1-3

--- level14.js
+++ level14.js
@@ -5,11 +5,20 @@ const arr = [3,5,6,2,3]
 // console.log(arrLength);
 
 //Q2.remove first two indexes // expected output: [6,2,3]
-let output = arr.splice(2, 3)
-console.log(output)
+// let output = arr.splice(2, 3)
+// console.log(output)
 
 
 const details = [
-    'ram',{fullMarks: 150}, {scores: [45,23,25]}
+    'ram', {fullMarks: 150}, {scores: [45,23,25]}
 ]
 //Q3. Find the percentage scored 
+const [fullname, {fullMarks}, {scores}] = details;
+
+let totalScore = 0;
+for (let i=0; i<scores.length; i++) {
+    totalScore += scores[i];
+}
+
+const percentage = (totalScore / fullMarks * 100);
+console.log(percentage);
