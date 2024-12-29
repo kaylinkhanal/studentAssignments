1ee9bdc506da086adbe9c42d67a898ec6dc5934d
Bijay Budha <programmerbijay1@gmail.com>
2024-12-29 09:13:46 +0545

    Slove array questions

--- level14.js
+++ level14.js
@@ -1,13 +1,23 @@
-const arr = [3,5,6,2,3]
+const arr = [3, 5, 6, 2, 3];
 
 //Q1. find length of arr
 
+console.log(arr.length);
 
 //Q2.remove first two indexes // expected output: [6,2,3]
 
+console.log(arr.splice(2));
 
-const details = [
-    'ram',{fullMarks: 150}, {scores: [45,23,25]}
-]
-//Q3. Find the percentage scored 
-console.log(details[2])
+const details = ["ram", { fullMarks: 150 }, { scores: [45, 23, 25] }];
+//Q3. Find the percentage scored
+
+const [
+  fullName,
+  { fullMarks },
+  {
+    scores: [subjectA, subjectB, subjectC],
+  },
+] = details;
+
+const markPercentage = ((subjectA + subjectB + subjectC) / fullMarks) * 100;
+console.log(markPercentage);
