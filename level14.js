17e2631ca1c24687638fc25598f9f81e02915909
Neha Thakur <tneha6260@gmail.com>
2024-12-29 22:50:56 +0545

    added answers of level14

--- level14.js
+++ level14.js
@@ -1,13 +1,20 @@
 const arr = [3,5,6,2,3]
 
 //Q1. find length of arr
+console.log(arr.length)
 
 
 //Q2.remove first two indexes // expected output: [6,2,3]
+console.log(arr.slice(2))
 
 
 const details = [
     'ram',{fullMarks: 150}, {scores: [45,23,25]}
 ]
 //Q3. Find the percentage scored 
-console.log(details[2])
+// console.log(details[2])
+
+const [fullName, {fullMarks}, {scores: subjectA, subjectB, subjectC}] = details
+totalScore = subjectA + subjectB + subjectC
+// const percentageScored = (totalScore / fullMarks) * 100
+console.log(totalScore)
