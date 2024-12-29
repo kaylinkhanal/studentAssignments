338b8407a8729dfdae2f6eaf5465aa104da917f5
Bibek Bhatta <bhattabbk1@gmail.com>
2024-12-29 08:54:25 +0545

    array answer

--- level14.js
+++ level14.js
@@ -2,12 +2,17 @@ const arr = [3,5,6,2,3]
 
 //Q1. find length of arr
 
+const arrLength = arr.length;
+console.log(arrLength);
+
 
 //Q2.remove first two indexes // expected output: [6,2,3]
 
+const removeIndexes = arr.splice(2);
+console.log(removeIndexes);
 
-const details = [
-    'ram',{fullMarks: 150}, {scores: [45,23,25]}
-]
-//Q3. Find the percentage scored 
-console.log(details[2])
+// const details = [
+//     'ram',{fullMarks: 150}, {scores: [45,23,25]}
+// ]
+// //Q3. Find the percentage scored 
+// console.log(details[2])
