fdf7b5f23ada863271435dcba2d2d7cff1591d65
rahulcoder2 <rahulchaudharycsit077@gmail.com>
2024-12-30 08:57:20 +0545

    add: level17 output

--- level17.js
+++ level17.js
@@ -8,7 +8,10 @@ const results = {
 //Q1 find  the all time highest score hint: use Math.max
 //expected ouput is 54
 
+const { 2023: scores2023, 2024: scores2024, 2025: scores2025 } = results;
 
+const output = Math.max(...scores2023, ...scores2024, ...scores2025);
+console.log(output)
 
 //Q2 remove 2025
 //exepcted output {
@@ -16,7 +19,11 @@ const results = {
 //     2024: [42,54,43],
 // }
 
+delete results[2025]
+console.log(results)
 
 //Q3 add all scores in a array
 // [23,12,32,42,54,43]
 
+const output3 = [...scores2023,...scores2024]
+console.log(output3)
