be6b4439e49f5f20c63648059eedcd9a89eaf896
rahulcoder2 <rahulchaudharycsit077@gmail.com>
2024-12-30 08:36:21 +0545

    add: level16 output

--- level16.js
+++ level16.js
@@ -6,12 +6,27 @@ const laps = ['10km', '12km', '13km']
 //   totalLap: 35
 // }
 
+const value1 = parseInt(laps[0])
+const value2 = parseInt(laps[1])
+const value3 = parseInt(laps[2])
+
+
+const output = {
+    totalLap: value1 + value2 + value3
+}
+console.log(output)
+
 const newLap = '14km'
 const newLap2 = '11km'
 // Q2. console.log laps, expected output 
 // ['10km', '12km', '13km', '14km','11km']
 
+laps.push(newLap,newLap2)
+console.log(laps)
 
 //Q3. pop last lap from the array
 //  console.log laps, expected output 
 // ['10km', '12km', '13km', '14km']
+
+const output3 = laps.pop()
+console.log(laps)
