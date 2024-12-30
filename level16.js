83e0435971b0a10137d18f6b7b04a32077e37cb4
Samirghimire-5 <samirghimire9841@gmail.com>
2024-12-30 08:31:40 +0545

    solved Q's

--- level16.js
+++ level16.js
@@ -6,12 +6,26 @@ const laps = ['10km', '12km', '13km']
 //   totalLap: 35
 // }
 
+let [lap1, lap2, lap3] = laps;
+lap1 = parseInt(lap1)
+lap2 = parseInt(lap2)
+lap3 = parseInt(lap3);
+let totalLap = lap1 + lap2 + lap3;
+console.log({totalLap});
+
+
+
 const newLap = '14km'
 const newLap2 = '11km'
 // Q2. console.log laps, expected output 
 // ['10km', '12km', '13km', '14km','11km']
+laps.push(newLap, newLap2);
+console.log(laps);
+
 
 
 //Q3. pop last lap from the array
 //  console.log laps, expected output 
 // ['10km', '12km', '13km', '14km']
+laps.pop();
+console.log(laps);
