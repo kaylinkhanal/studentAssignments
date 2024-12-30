117d231bbf881543e97271278a20c580ffda5a59
Samirghimire-5 <samirghimire9841@gmail.com>
2024-12-30 08:47:52 +0545

    solved Q's

--- level17.js
+++ level17.js
@@ -7,6 +7,9 @@ const results = {
 
 //Q1 find  the all time highest score hint: use Math.max
 //expected ouput is 54
+const {2023: year1, 2024: year2, 2025: year3} = results;
+let highestScore = Math.max(...year1, ...year2, ...year3)
+console.log(highestScore);
 
 
 
@@ -16,7 +19,12 @@ const results = {
 //     2024: [42,54,43],
 // }
 
+let resul = {...results}
+delete resul[2025];
+console.log(resul);
 
 //Q3 add all scores in a array
 // [23,12,32,42,54,43]
 
+let allScores = [...results[2023], ...results[2024], ...results[2025]];
+    console.log(allScores);
