b729c89e600c13afcd6498b4757a1d46a8b6b838
rahulcoder2 <rahulchaudharycsit077@gmail.com>
2024-12-30 09:13:23 +0545

    modifiy: level17 output2

--- level17.js
+++ level17.js
@@ -19,8 +19,14 @@ console.log(output)
 //     2024: [42,54,43],
 // }
 
-delete results[2025]
-console.log(results)
+// not delete but removing 
+
+console.log({scores2023, scores2024})
+
+
+// one way delete but no going backward 
+// delete results[2025]
+// console.log(results)
 
 //Q3 add all scores in a array
 // [23,12,32,42,54,43]
