05396750ed6db420c4a3e922d7cf1c7b88b54cfc
rupak-at <rupakb802@gmail.com>
2024-12-30 08:25:43 +0545

    Q3 solve

--- level16.js
+++ level16.js
@@ -5,13 +5,23 @@ const laps = ['10km', '12km', '13km']
 // {
 //   totalLap: 35
 // }
+const[first, second, third] = laps
+const firstNum = first.slice(0,-2)
+const secondNum = second.slice(0,-2)
+const thirdNum = third.slice(0,-2)
+console.log(Number(firstNum)+Number(secondNum)+Number(thirdNum))
 
 const newLap = '14km'
 const newLap2 = '11km'
 // Q2. console.log laps, expected output 
 // ['10km', '12km', '13km', '14km','11km']
+laps.push(newLap)
+laps.push(newLap2)
+console.log(laps)
 
 
 //Q3. pop last lap from the array
 //  console.log laps, expected output 
 // ['10km', '12km', '13km', '14km']
+laps.pop()
+console.log(laps)
