74df6d5965ce0cdaf2fd05fadaf35152415b6f61
Prem kala <114562215+Premkalagithub@users.noreply.github.com>
2024-12-30 08:40:22 +0545

    added tasks

--- level16.js
+++ level16.js
@@ -5,7 +5,16 @@ const laps = ['10km', '12km', '13km']
 // {
 //   totalLap: 35
 // }
-console.log()
+// const num1=parseInt(laps[0])
+// const num2=parseInt(laps[1])
+// const num3=parseInt(laps[2])
+console.log({
+    totallap:parseInt(laps[0])+ parseInt(laps[1]) +parseInt(laps[2])
+})
+
+
+
+
 const newLap = '14km'
 const newLap2 = '11km'
 // Q2. console.log laps, expected output 
