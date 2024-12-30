0dbe7e1d9ee7564a88fd6cef6382edc4f985f8fa
Bijay Budha <programmerbijay1@gmail.com>
2024-12-30 08:02:53 +0545

    sloved level15.js

--- level15.js
+++ level15.js
@@ -1,21 +1,27 @@
 const details = {
-    user: {
-        name: 'ram khanal'
-    },
-    subjects: ['math','science']
-}
+  user: {
+    name: "ram khanal",
+  },
+  subjects: ["math", "science"],
+};
 
-const subjects= {
-    math: 35,
-    science: 36,
-    computer: 56
-}
+const subjects = {
+  math: 35,
+  science: 36,
+  computer: 56,
+};
 
-
-//Expected output: 
+//Expected output:
 // {
 //     user: {
 //         name: 'ram khanal'
 //     },
 //     subjects: [35,36]
 // }
+
+let nestedObj = {
+  user: details.user.name,
+  subjects: [subjects.math, subjects.science],
+};
+
+console.log(nestedObj);
