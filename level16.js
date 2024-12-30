931ddb973736bef45a06c26bc37fb55baac93d8f
Hema Pandey <pandeyhema788@gmail.com>
2024-12-30 08:34:59 +0545

    complete questions

--- level16.js
+++ level16.js
@@ -6,12 +6,25 @@ const laps = ['10km', '12km', '13km']
 //   totalLap: 35
 // }
 
+const firstnumber=parseInt(laps[0])
+const secondenumber=parseInt(laps[1])
+const thirdnumber=parseInt(laps[2])
+const sum={
+    totallap:firstnumber+secondenumber+thirdnumber
+}
+console.log(sum)
+
 const newLap = '14km'
 const newLap2 = '11km'
 // Q2. console.log laps, expected output 
 // ['10km', '12km', '13km', '14km','11km']
 
+let pushlaps=laps.push(newLap,newLap2)
+console.log(pushlaps)
+console.log(laps)
 
 //Q3. pop last lap from the array
 //  console.log laps, expected output 
 // ['10km', '12km', '13km', '14km']
+ const popitem=laps.pop(newLap2)
+console.log (laps)
