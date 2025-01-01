8df81587302569b825ca61295f711790f9719dca
Hema Pandey <pandeyhema788@gmail.com>
2025-01-01 09:08:03 +0545

    added task

--- level18.js
+++ level18.js
@@ -1,7 +1,11 @@
 let pocketMoney= 200
-function buyChocolate(price, chocolate){
-    const buyCount = Math.floor(pocketMoney / price)
-    return `You can buy  ${buyCount}  ${chocolate}  with your pocket money`
+function buyChocolate(price, chocolate,quantity){
+    // const buyCount = Math.floor(pocketMoney/ price)
+    // return `You can buy  ${buyCount}  ${chocolate}  with your pocket money`
+
+    pocketMoney=pocketMoney-(price*quantity)
+    return `you have ${pocketMoney} RS. remaining`
+  
 }
 // console.log(buyChocolate(11, 'kitkat')) //arguments
 // console.log(buyChocolate(22, 'dairymilk'))
@@ -22,3 +26,14 @@ console.log(buyChocolate(20, 'dairymilk',4))
 //expected output is 
 // You have 170 Rs remaining
 // You have 90 Rs remaining
+
+
+//Q3. convert into arrow function
+
+function callMe(){
+    return 'hi'
+}
+
+const callme=()=>{
+    return 'hi'
+}
