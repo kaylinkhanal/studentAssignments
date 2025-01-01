7ca98041934e3be92a45a574de5ebdd53583bef1
Ajay Gurung <gooronz.ajay@gmail.com>
2025-01-01 12:40:40 +0545

    level 18 q1 and q2 done

--- level18.js
+++ level18.js
@@ -1,7 +1,14 @@
 let pocketMoney= 200
-function buyChocolate(price, chocolate){
-    const buyCount = Math.floor(pocketMoney / price)
-    return `You can buy  ${buyCount}  ${chocolate}  with your pocket money`
+function buyChocolate(price, chocolate, quantity){
+    if(quantity == undefined){
+        const buyCount = Math.floor(pocketMoney / price)
+        return `You can buy  ${buyCount}  ${chocolate}  with your pocket money`
+    }
+    else{
+        total_cost = price * quantity
+        remaining_am = pocketMoney - total_cost
+        return `You have ${remaining_am} Rs remaining`
+    }
 }
 // console.log(buyChocolate(11, 'kitkat')) //arguments
 // console.log(buyChocolate(22, 'dairymilk'))
@@ -9,6 +16,8 @@ function buyChocolate(price, chocolate){
 //Q1. expected output: 
 //You can buy 20 kitkat with your pocketmoney
 // You can buy 10 dairymilk with your pocketmoney
+console.log(buyChocolate(10, 'kitkat'))
+console.log(buyChocolate(20, 'dairymilk'))
 
 //Q2. 
 console.log(buyChocolate(10, 'kitkat', 3)) //arguments
@@ -16,3 +25,5 @@ console.log(buyChocolate(20, 'dairymilk',4))
 //expected output is 
 // You have 170 Rs remaining
 // You have 90 Rs remaining
+
+
