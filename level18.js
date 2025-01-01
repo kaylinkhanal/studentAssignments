38197a8450ad8e22127c8856fb4413d8793bd543
Sujan1567 <np01cp4s220189@islingtoncollege.edu.np>
2025-01-01 08:54:25 +0545

    function solution1

--- level18.js
+++ level18.js
@@ -1,8 +1,23 @@
 let pocketMoney = 200;
 
+// function buyChocolate(price, chocolate) {
+//   const remainingAmount = pocketMoney / price;
+//   return (
+//     "you can buy " +
+//     parseInt(remainingAmount) +
+//     " " +
+//     chocolate +
+//     " " +
+//     "with your pocket money"
+//   );
+// }
+
+//Q1. expected output:
+//You can buy 20 kitkat with your pocketmoney
+// You can buy 10 dairymilk with your pocketmoney
+
 function buyChocolate(price, chocolate) {
   const remainingAmount = pocketMoney / price;
-  // return ' you buy cholocate' + chocolate + 'you will have '+ remainingAmount
   return (
     "you can buy " +
     parseInt(remainingAmount) +
@@ -11,12 +26,11 @@ function buyChocolate(price, chocolate) {
     " " +
     "with your pocket money"
   );
-  //  return 'you can buy'+ remainingAmount+ chocolate+ 'with your pocket money'
 }
 
 console.log(buyChocolate(11, "kitkat")); //arguments
 console.log(buyChocolate(20, "dairymilk"));
 
-//Q1. expected output:
-//You can buy 20 kitkat with your pocketmoney
-// You can buy 10 dairymilk with your pocketmoney
+//Q2. expected output:
+//you have 170 Rs remaining
+//you have 90 Rs remaining
