9cb50d57d86d7b8f70a778f6fe1a447472a97003
Kaylin Khanal <kyalinnop@gmail.com>
2025-01-01 08:52:31 +0545

    fix: add question

--- level18.js
+++ level18.js
@@ -1,15 +1,18 @@
 let pocketMoney= 200
-
 function buyChocolate(price, chocolate){
-    const remainingAmount = pocketMoney - price
-    return 'If you buy cholocate' + chocolate + 'you will have '+ remainingAmount
+    const buyCount = Math.floor(pocketMoney / price)
+    return `You can buy  ${buyCount}  ${chocolate}  with your pocket money`
 }
-
-console.log(buyChocolate(10, 'kitkat')) //arguments
-console.log(buyChocolate(20, 'dairymilk'))
-
-
+// console.log(buyChocolate(11, 'kitkat')) //arguments
+// console.log(buyChocolate(22, 'dairymilk'))
 
 //Q1. expected output: 
 //You can buy 20 kitkat with your pocketmoney
 // You can buy 10 dairymilk with your pocketmoney
+
+//Q2. 
+console.log(buyChocolate(10, 'kitkat', 3)) //arguments
+console.log(buyChocolate(20, 'dairymilk',4))
+//expected output is 
+// You have 170 Rs remaining
+// You have 90 Rs remaining
