a3dc17bfd2097753fcd33b5c46b5db1852f15019
Hema Pandey <pandeyhema788@gmail.com>
2025-01-01 08:48:45 +0545

    added task

--- level18.js
+++ level18.js
@@ -1,12 +1,17 @@
 let pocketMoney= 200
 
 function buyChocolate(price, chocolate){
-    const  totalchoclate=pocketMoney/price
-    return ' you can buy' + totalchoclate + chocolate + 'with your pocket money'
+    const  totalchoclate=Math.floor(pocketMoney/price)
+    return ` you can buy ${totalchoclate}  ${chocolate } with your pocket money`
 }
 
-console.log(buyChocolate(10, 'kitkat')) //arguments
+console.log(buyChocolate(11, 'kitkat')) //arguments
 console.log(buyChocolate(20, 'dairymilk'))
+
+
+
+
+
 
 
 
