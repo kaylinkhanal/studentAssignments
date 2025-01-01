a47359bec89ca4a94d6382f8bd82e08d435c6151
Ajay Gurung <gooronz.ajay@gmail.com>
2025-01-01 08:41:12 +0545

    level 16 and 17 done

--- level17.js
+++ level17.js
@@ -8,6 +8,10 @@ const results = {
 //Q1 find  the all time highest score hint: use Math.max
 //expected ouput is 54
 
+//combining all arrays
+let all_scores = [results['2023'], results['2024'], results['2025']].flat()
+let max = Math.max(...all_scores)
+console.log(max)
 
 
 //Q2 remove 2025
@@ -15,8 +19,11 @@ const results = {
 //     2023: [23,12,32],
 //     2024: [42,54,43],
 // }
+delete(results['2025'])
+console.log(results)
 
 
 //Q3 add all scores in a array
 // [23,12,32,42,54,43]
-
+let remaining_scores = [results['2023'], results['2024']].flat()
+console.log(remaining_scores)
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+all_scores.push()
