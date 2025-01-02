02dc7013c804caf1e57260adea64c69c3e54eb85
Kaylin Khanal <kyalinnop@gmail.com>
2025-01-02 09:19:02 +0545

    added level 19 answers

--- level19.js
+++ level19.js
@@ -1,15 +1,23 @@
 const arr = [3,5,6,1, 'ram' ,false]
 
 let sum = 0
-const output = arr.map((item)=>{
-    //sum = sum + item
+const output = arr.forEach((item)=>{
+    sum = sum + (parseInt(item) || 0)
 })
 console.log(sum)
 
 //q1. calulate the sum of Numbers
 //q2. filter out non integers : expected output : [3,5,6,1]
+const filtered = arr.filter((item)=>{
+   return parseInt(item)
+})
+console.log(filtered)
 //q3. in the filtered result, multiply each number by 5 using .map
+const mapped = filtered.map((item)=>{
+    return item * 5
+})
 
+console.log(mapped)
 
 
 // MAP                   vs                   FOREACH
