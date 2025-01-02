ebf850c742eb6e87488ad03147bf5f7c8127c9d2
Sujan1567 <np01cp4s220189@islingtoncollege.edu.np>
2025-01-02 09:00:24 +0545

    solving the problems

--- level19.js
+++ level19.js
@@ -1,8 +1,8 @@
-const arr = [3,5,6,1]
-let sum = 0
-arr.forEach((item)=>{
-    sum= sum + item
-})
-console.log(sum)
-
-// calculate sum of odd number
+const arr = [3, 5, 6, 1];
+let sum = 0;
+arr.forEach((item) => {
+  if (item % 2 == 1) {
+    sum = sum + item;
+  }
+});
+console.log(sum);
