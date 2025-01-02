8d42164f30cd830f822c5be61f4faddde7c5e05c
Ajay Gurung <gooronz.ajay@gmail.com>
2025-01-02 12:44:48 +0545

    level 19 done forEach, map, filter

--- level19.js
+++ level19.js
@@ -1,23 +1,36 @@
 const arr = [3,5,6,1, 'ram' ,false]
 
+// let sum = 0
+// const output = arr.forEach((item)=>{
+//     sum = sum + (parseInt(item) || 0)
+// })
+// console.log(sum)
+
+//q1. calulate the sum of Numbers
 let sum = 0
-const output = arr.forEach((item)=>{
+arr.forEach((item) => {
     sum = sum + (parseInt(item) || 0)
 })
 console.log(sum)
 
-//q1. calulate the sum of Numbers
 //q2. filter out non integers : expected output : [3,5,6,1]
-const filtered = arr.filter((item)=>{
-   return parseInt(item)
+const integers = arr.filter((item) => {
+    return parseInt(item)
 })
-console.log(filtered)
+console.log(integers)
+ 
 //q3. in the filtered result, multiply each number by 5 using .map
-const mapped = filtered.map((item)=>{
+const mul5 = integers.map((item) => {
     return item * 5
 })
-
-console.log(mapped)
+console.log(mul5)
+// ALT: using function chaining
+const ans = arr.filter((item) => {
+    return parseInt(item)
+}).map((item) => {
+    return item * 5
+})
+console.log(ans)
 
 
 // MAP                   vs                   FOREACH
