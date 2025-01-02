abe2648c1c8719c97f75da07738880645476fe5e
Hema Pandey <pandeyhema788@gmail.com>
2025-01-02 09:19:30 +0545

    added task

--- level19.js
+++ level19.js
@@ -1,12 +1,25 @@
 const arr = [3,5,6,1, 'ram' ,false]
 
-let sum = 0
-const output = arr.map((item)=>{
-    //sum = sum + item
-})
-console.log(sum)
+// let sum = 0
+// const output = arr.map((item)=>{
+//     //sum = sum + item
+// })
+// console.log(sum)
 
 //q1. calulate the sum of Numbers
+
+
+const num = arr.filter((item)=>{
+    if (Number.isInteger(item)){
+
+    }
+})
+
+let sumNum=0
+const sum=num.forEach((item)=>{
+sumNum=item
+})
+console.log(sumNum)
 //q2. filter out non integers : expected output : [3,5,6,1]
 //q3. in the filtered result, multiply each number by 5 using .map
 
