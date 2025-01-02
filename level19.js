57754dcc7a873f76e1f90b1dadf8247b4f05eb51
parashsubedi27@gmail.com <parashsubedi37@gmail.com>
2025-01-02 09:02:52 +0545

    function

--- level19.js
+++ level19.js
@@ -1,8 +1,34 @@
-const arr = [3,5,6,1]
-let sum = 0
-arr.forEach((item)=>{
-    sum= sum + item
-})
-console.log(sum)
+
+// console.log(arr);
+
 
 // calculate sum of odd number
+// const arr = [3,5,6,1]
+// let sum = 0
+// arr.forEach((item)=>{
+//     if(item%2===1){
+//         sum= sum + item
+//     }else{
+        
+//     }
+    
+// })
+// console.log(sum)
+
+// const arr=[3,5,6,1,'ram',false]
+// let sum =0;
+// const output=arr.map((item))=>{
+//     sum=sum+item;
+// }
+
+
+//Q1 calculate the sum of numbers
+
+const arr=[3,5,6,1,'ram',false]
+let sum =0;
+const output=arr.filter((item)=>{
+    sum=sum+item;
+})
+console.log(sum);
+
+//Q2
