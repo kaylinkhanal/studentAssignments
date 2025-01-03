b2c33f441513d8ee03f268ac9e7d23faf1ea20c0
Ajay Gurung <gooronz.ajay@gmail.com>
2025-01-03 09:02:09 +0545

    level21 done

---
+++ level21.js
@@ -0,0 +1,21 @@
+const scores = [
+    ['math',99], ['science',93], ['computer',95]
+]
+
+//Q1. calculate the percentage scored
+let total = 0
+
+scores.forEach((item) => {
+    total += item[1]
+})
+let percentage = total/300*100
+console.log(percentage.toFixed(2))
+
+
+//Q2. What is the lowest marks scored? expected output: 93
+let scores_arr = []
+scores.forEach((item) => {
+    scores_arr.push(item[1])
+})
+
+console.log(Math.min(...scores_arr))
