71baf553c4ba04458310d6774b2309adeb49eabd
ipradeepshah <ipradeepshah@gmail.com>
2025-01-03 09:23:58 +0545

    solved level 21

--- level21.js
+++ level21.js
@@ -6,10 +6,28 @@ const scores = [
 
 let total = 0
 scores.forEach((item)=>{
+    total = total + item[1]
+
     // harek ko item[1] lai add to total
+   
 })
+const percent =(total /  300) * 100
+ 
+console.log (percent)
 
 // const percentage = total divide by ......
 //console.log(percentage)
 //Q2 what is the lowest marks scored? expected ouptut: 93
 
+// let lowest = 100
+// scores.forEach((item)=>{
+//     if(item[1] < lowest){
+//         lowest = item[1]
+//     }
+// })
+// console.log(lowest) 
+
+const onlyScore = scores.map((item)=>{
+    return item[1]
+})
+console.log(Math.min(...onlyScore))
