3d2fd045f4fb22e478a72e3c28aa41a441216c97
Bibek Bhatta <bhattabbk1@gmail.com>
2025-01-03 08:30:24 +0545

    add foreach loop

--- level20.js
+++ level20.js
@@ -32,7 +32,7 @@ console.log(output)
 // output is 70
 // hint
 let ageSum = 0
-arr.map((item)=>{
+arr.forEach((item)=>{
     // ageSum = ageSum  + item ko age
     return ageSum = ageSum + item.age; 
     })
