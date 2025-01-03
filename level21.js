12a1b61ff70b194adab1d7d3177e1ea930c8f311
Bijay Budha <programmerbijay1@gmail.com>
2025-01-03 09:26:13 +0545

    slove Q2. level21.js

--- level21.js
+++ level21.js
@@ -1,15 +1,27 @@
 const scores = [
-   [ 'math',99],[ 'science',93], ['computer',95]
-]
+  ["math", 99],
+  ["science", 93],
+  ["computer", 95],
+];
 
 //Q1. calculate the percentage scored
 
-let total = 0
-scores.forEach((item)=>{
-    // harek ko item[1] lai add to total
-})
+let total = 0;
+scores.forEach((item) => {
+  total += item[1];
+  total = (total / 300) * 100;
+  return total;
+});
+
+console.log(total);
 
 // const percentage = total divide by ......
 //console.log(percentage)
 //Q2 what is the lowest marks scored? expected ouptut: 93
 
+const lowestNumber = scores.map((item) => {
+  return item[1];
+});
+
+const result = Math.min(...lowestNumber);
+console.log(result);
