ce22632612a6aded6bc8e3f3064f7243d61581ee
=raj-maharjan99 <=rjuallone@gmail.com>
2025-01-02 20:37:44 -0600

    nothing

--- level19.js
+++ level19.js
@@ -1,8 +1,16 @@
-const arr = [3,5,6,1]
-let sum = 0
-arr.forEach((item)=>{
-    sum= sum + item
-})
-console.log(sum)
+const arr = [3, 5, 6, 1];
+
+//   return item % 2 !== 0;
+
+// console.log(oddNumber);
+// sum = oddNumber.reduce((a, b) => a + b, 0);
 
 // calculate sum of odd number
+// console.log(sum);
+
+const out = arr
+  .map((item) => item + 1)
+  .map((a) => a + 2)
+  .filter((b) => b % 2 == 0)
+  .map((a) => a + 5);
+console.log(out);
