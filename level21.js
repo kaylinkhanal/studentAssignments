bdc93d5fec57352689e33892ad628c19f147a245
=raj-maharjan99 <=rjuallone@gmail.com>
2025-01-02 21:36:50 -0600

    level 21 solved

--- level21.js
+++ level21.js
@@ -1,15 +1,24 @@
 const scores = [
-   [ 'math',99],[ 'science',93], ['computer',95]
-]
+  ["math", 99],
+  ["science", 93],
+  ["computer", 95],
+];
 
 //Q1. calculate the percentage scored
 
-let total = 0
-scores.forEach((item)=>{
-    // harek ko item[1] lai add to total
-})
+let total = 0;
+scores.forEach((item) => {
+  total += item[1];
+});
 
+const totalMarks = scores.length * 100;
+console.log(
+  "QN 1 :- percentage scored :",
+  ((total / totalMarks) * 100).toFixed(2)
+);
 // const percentage = total divide by ......
 //console.log(percentage)
 //Q2 what is the lowest marks scored? expected ouptut: 93
 
+const lowestScore = scores.map((num) => num[1]);
+console.log("QN 2 ", Math.min(...lowestScore));
