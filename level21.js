53903e97965e6a83d79782ee813e943596cff4f8
parashsubedi27@gmail.com <parashsubedi37@gmail.com>
2025-01-05 08:12:11 +0545

    function

--- level21.js
+++ level21.js
@@ -6,13 +6,19 @@ const scores = [
 
 let total = 0
 scores.forEach((item)=>{
-    total+=item[1]
-    
+    total = total + item[1]
 })
 console.log(total);
 
+console.log(total/3)
 
-const percentage = (total/300)*100
-console.log(percentage)
+// const percentage = total divide by ......
+//console.log(percentage)
 //Q2 what is the lowest marks scored? expected ouptut: 93
 
+
+const output2 = scores.map((item)=>{
+    return item[1]
+})
+
+const result = Math.min(...output2)
