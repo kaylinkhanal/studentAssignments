9b3b5b4d0fe483fb37b59f760911ea36ffc35d2d
Bijay Budha <programmerbijay1@gmail.com>
2025-01-05 08:33:35 +0545

    sloving level22ai.js Q4 & Q5

--- level22ai.js
+++ level22ai.js
@@ -12,13 +12,16 @@ const sentence = "the quick brown fox jumps over the lazy dog";
 //   dog: 1
 // }
 
+// const spiltSentence = sentence.split(" ");
+
+// spiltSentence.filter((item) => {});
 
 const phoneNumber = "123-456-7890";
 
 // Expected output:
 // "(123) 456-7890"
 
-
+// console.log(phoneNumber.split("-"));
 
 const email = "test.user@example.com";
 
@@ -28,30 +31,29 @@ const email = "test.user@example.com";
 //   domain: "example.com"
 // }
 
-
-
-
 const words = ["apple", "banana", "apricot", "kiwi", "avocado"];
 
 // Expected output:
 // ["apple", "apricot", "avocado"] (all words starting with "a")
 
+const startingALetter = words.filter((item) => {
+  if (item.charAt(0) === "a") return item;
+  //   console.log(item);
+});
 
+console.log(startingALetter);
 
 const mixedCase = "HeLlO wOrLd";
 
 // Expected output:
 // "hello world" (all lowercase)
 
-
+console.log(mixedCase.toLocaleLowerCase());
 
 const longString = "This is a very long string with multiple words.";
 
 // Expected output:
 // 7 (number of words)
-
-
-
 
 const phrase = "racecar";
 
