ca05aa0ca0c868c5015edda89f4c23d8372a1961
parashsubedi27@gmail.com <parashsubedi37@gmail.com>
2025-01-06 09:11:03 +0545

    function

--- level22ai.js
+++ level22ai.js
@@ -1,72 +1,81 @@
-const sentence = "the quick brown fox jumps over the lazy dog";
+// const sentence = "the quick brown fox jumps over the lazy dog";
 
-// Expected output:
-// {
-//   the: 2,
-//   quick: 1,
-//   brown: 1,
-//   fox: 1,
-//   jumps: 1,
-//   over: 1,
-//   lazy: 1,
-//   dog: 1
-// }
-
-
-const phoneNumber = "123-456-7890";
+// // Expected output:
+// // {
+// //   the: 2,
+// //   quick: 1,
+// //   brown: 1,
+// //   fox: 1,
+// //   jumps: 1,
+// //   over: 1,
+// //   lazy: 1,
+// //   dog: 1
+// // }
 
 
-// Expected output:
-// "(123) 456-7890"
+// const phoneNumber = "123-456-7890";
 
 
-
-const email = "test.user@example.com";
-console.log({
-    username:email.split('@')[0],
-    domain:email.split('@')[1],
-})
-// Expected output:
-// {
-//   username: "test.user",
-//   domain: "example.com"
-// }
+// // Expected output:
+// // "(123) 456-7890"
 
 
 
-
-const words = ["apple", "banana", "apricot", "kiwi", "avocado"];
-
-// Expected output:
-// ["apple", "apricot", "avocado"] (all words starting with "a")
-
-const output =words.filter((items)=>{
-    if(items[0]==='a'){
-      return items
-    }
-})
+// const email = "test.user@example.com";
+// console.log({
+//     username:email.split('@')[0],
+//     domain:email.split('@')[1],
+// })
+// // Expected output:
+// // {
+// //   username: "test.user",
+// //   domain: "example.com"
+// // }
 
 
 
 
-const mixedCase = "HeLlO wOrLd";
+// const words = ["apple", "banana", "apricot", "kiwi", "avocado"];
 
-// Expected output:
-// "hello world" (all lowercase)
-console.log(mixedCase.toLowerCase());
+// // Expected output:
+// // ["apple", "apricot", "avocado"] (all words starting with "a")
 
-
-
-const longString = "This is a very long string with multiple words.";
-
-// Expected output:
-// 7 (number of words)
+// const output =words.filter((items)=>{
+//     if(items[0]==='a'){
+//       return items
+//     }
+// })
 
 
 
 
-const phrase = "racecar";
-console.log(phrase.split('').reverse().join()==phrase)
+// const mixedCase = "HeLlO wOrLd";
 
-// Expected output:
-// true (check if the string is a palindrome)
+// // Expected output:
+// // "hello world" (all lowercase)
+// console.log(mixedCase.toLowerCase());
+
+
+
+// const longString = "This is a very long string with multiple words.";
+
+// // Expected output:
+// // 7 (number of words)
+
+
+
+
+// const phrase = "racecar";
+// console.log(phrase.split('').reverse().join()==phrase)
+
+// // Expected output:
+// // true (check if the string is a palindrome)
+
+// const orderTotals = [342, 1002, 523, 34, 634, 854, 1644, 2222];
+// function tallyNumbers(tally, currentTotal) {
+//     console.log(`The current tally is ${tally}`);
+//     console.log(`The current total is ${currentTotal}`);
+//     console.log('----------');
+//     return tally + currentTotal;
+//   }
+//   const allOrders = orderTotals.reduce(tallyNumbers,0);
