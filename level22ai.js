e379caf549a37c5255006a09513499b6475494a9
Hema Pandey <pandeyhema788@gmail.com>
2025-01-08 08:15:39 +0545

    added task

--- level22ai.js
+++ level22ai.js
@@ -17,23 +17,12 @@ splittedSentence.forEach((item)=>{
 //   lazy: 1,
 //   dog: 1
 // }
-<<<<<<< HEAD
-const splittedSetence=sentence.split(' ')
-let temMap={}
-splittedSetence.forEach((item)=>{
-    temMap[item]=temMap[item] ? ++temMap[item]:1
-})
-console.log(temMap)
-
-=======
 //hint 1 : get something like: 
 // ['the', 'quick' ,'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog];
 //hint 2: loop the array
->>>>>>> d1aa61cb1656516302a136e89076af313da6f736
 
 //Q2.
 const phoneNumber = "123-456-7890";
-<<<<<<< HEAD
 const[areaCode,prefix,lineNumber]=phoneNumber.split('-')
 console.log(`(${areaCode}) ${prefix}-${lineNumber}`)
 
@@ -44,13 +33,6 @@ console.log(`(${areaCode}) ${prefix}-${lineNumber}`)
 
 
 //Q3.
-=======
-const [areaCode, prefix, lineNumber] = phoneNumber.split('-')
-console.log(`(${areaCode}) ${prefix}-${lineNumber}`)
-// Expected output:
-// "(123) 456-7890"
-
->>>>>>> d1aa61cb1656516302a136e89076af313da6f736
 const email = "test.user@example.com";
 // Expected output:
 // {
@@ -58,7 +40,6 @@ const email = "test.user@example.com";
 //   domain: "example.com"
 // }
 console.log({
-<<<<<<< HEAD
     userName:email.split('@')[0],
     domain:email.split('@')[1],
 })
@@ -66,17 +47,10 @@ console.log({
 
 
 //Q4.
-=======
-    userName: email.split('@')[0],
-    domain: email.split('@')[1],
-})
-
->>>>>>> d1aa61cb1656516302a136e89076af313da6f736
 const words = ["apple", "banana", "apricot", "kiwi", "avocado"];
 
 // Expected output:
 // ["apple", "apricot", "avocado"] (all words starting with "a")
-<<<<<<< HEAD
 
 
 const output = words.filter((item)=>{
@@ -88,12 +62,6 @@ console.log(output)
 
 
 //Q5.
-=======
-const output = words.filter((item)=>{
-if(item[0] === 'a') return item
-})
-console.log(output)
->>>>>>> d1aa61cb1656516302a136e89076af313da6f736
 
 const mixedCase = "HeLlO wOrLd";
 console.log(mixedCase.toLowerCase())
@@ -101,7 +69,6 @@ console.log(mixedCase.toLowerCase())
 // Expected output:
 // "hello world" (all lowercase)
 
-<<<<<<< HEAD
 console.log(mixedCase.toLowerCase())
 
 
@@ -112,11 +79,8 @@ console.log(mixedCase.toLowerCase())
 
 //Q6.
 
-=======
->>>>>>> d1aa61cb1656516302a136e89076af313da6f736
 const phrase = "racecar";
 console.log(phrase.split('').reverse().join('') === phrase)
 // Expected output:
 // true (check if the string is a palindrome)
  console.log(phrase.split('').reverse().join('')===phrase)
- 
