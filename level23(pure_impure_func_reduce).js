d5d87f42cbb43c1c4dcf5f8390fe79defccfa964
Ajay Gurung <gooronz.ajay@gmail.com>
2025-01-10 08:59:56 +0545

    level23 for_in_for_of

---
+++ level23(pure_impure_func_reduce).js
@@ -0,0 +1,49 @@
+
+// // PURE VS IMPURE FUNCTION IN JAVASCRIPT
+// // PURE:
+// // 1. Same output for same input 
+// // 2. this function depends on input parameters
+// // 3. Only uses its own scopes
+// // 4. Testing/Debugging is easier
+// // should have return
+
+
+// // IMPURE:
+// // 1. side efffect, modifies state: modifies variaibles in other/global scope
+// // 2. Depends on external factors/resources
+// // // loop over an array and save each item to database
+
+// function functionincrement(count) {
+//   count += 1; 
+//     console.log(count)
+// }
+
+// functionincrement(0)
+
+// // MAP --> array 
+// //FILTER --> array 
+
+// //REDUCE
+// 1. can return a single value, [], {}
+// 2. combine, transform as well as aggregate
+// 3. accumulator ---> a storable value in  each iteration
+// 4. 
+
+
+//Q
+const cart = [
+    {id:3, items:'hawkins cooker', quantity:3, price: 1000},
+    {id:4, items:'baltra cooker', quantity:2, price: 4000},
+]
+
+//expected output is 11000
+
+const output = cart.reduce((accumulator, currentItem, currentIndex) => {
+    debugger;
+    return accumulator + (cart[currentIndex].quantity * cart[currentIndex].price)
+    debugger;
+}, 0)
+
+console.log(output)
+
+
--- level23.js
+++ level23.js
@@ -8,12 +8,19 @@ const obj = {
   comp: 4
 }
 
-//O O
-for (let item in obj){
-  console.log(item)
-}
+//O O not possible O I possible
+// for (let item in obj){
+//   console.log(item)
+// }
 
 //Calculate the sum of all score
+
+let sum = 0
+for (score in obj){
+    sum += obj[score]
+}
+console.log(sum)
+
 
 
 const arr =[{ram:40},{gopal:50},{hari:60}]
@@ -21,3 +28,19 @@ const arr =[{ram:40},{gopal:50},{hari:60}]
 // using for of/for in output the highest marks scorer
 //expected ouput:
 //hari
+
+let highest_score = 0   // to find highest_score by comparing with each individual with each loop iterations
+let highest_scorer = ''
+
+// to find highest score. If at each iteration, new highest score is found, the highest_score gets updated
+for (item of arr){
+    for (person in item){    // to get the property name of current object
+        if (item[person] > highest_score){  //comparing score of current person with current highest_sore
+            highest_score = item[person]
+            highest_scorer = person
+        }
+    }
+}
+
+console.log(highest_score)
+console.log(highest_scorer)
