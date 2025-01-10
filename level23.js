feec2e06b380ac225a5ab87be9e5c60b5b339586
Sujan1567 <np01cp4s220189@islingtoncollege.edu.np>
2025-01-10 08:52:24 +0545

    solving the problem of level23

--- level23.js
+++ level23.js
@@ -1,14 +1,11 @@
 // // PURE VS IMPURE FUNCTION IN JAVASCRIPT
 
-
-
 // // PURE:
-// // 1. Same output for same input 
+// // 1. Same output for same input
 // // 2. this function depends on input parameters
 // // 3. Only uses its own scopes
 // // 4. Testing/Debugging is easier
 // // should have return
-
 
 // // IMPURE:
 // // 1. side efffect, modifies state: modifies variaibles in other/global scope
@@ -16,26 +13,25 @@
 // // // loop over an array and save each item to database
 
 // function functionincrement(count) {
-//   count += 1; 
+//   count += 1;
 //     console.log(count)
 // }
 
 // functionincrement(0)
 
-// // MAP --> array 
-// //FILTER --> array 
+// // MAP --> array
+// //FILTER --> array
 
 // //REDUCE
 // 1. can return a single value, [], {}
 // 2. combine, transform as well as aggregate
 // 3. accumulator ---> a storable value in  each iteration
-// 4. 
+// 4.
 
-
-const cart = [
-    {id: 3, item: 'hawkins cooker', quantity: 3, price: 1000},
-    {id: 4, item: 'baltra cooker', quantity: 2, price: 4000},
-]
+// const cart = [
+//   { id: 3, item: "hawkins cooker", quantity: 3, price: 1000 },
+//   { id: 4, item: "baltra cooker", quantity: 2, price: 4000 },
+// ];
 
 // let sum = 0
 // cart.forEach((item)=>{
@@ -44,12 +40,39 @@ const cart = [
 // console.log(sum)
 
 //expected output: 11000
- const output  =cart.reduce((sum, nextItem,nextId)=>{
-   sum = sum + nextItem
-   return sum
-},0)
+// const output = cart.reduce((sum, nextItem, nextId) => {
+//   sum = sum + nextItem;
+//   return sum;
+// }, 0);
 
+// console.log(output);
 
-console.log(output)
+// const obj = {
+//   math: 3,
+//   science: 5,
+//   comp: 4,
+// };
 
+//O O
+// let sum = 0;
+// for (let item in obj) {
+//   sum = sum + obj[item];
+// }
+// console.log(sum);
 
+//Calculate the sum of all score
+
+const arr = [{ ram: 40 }, { gopal: 50 }, { hari: 60 }];
+
+// using for of/for in output the highest marks scorer
+//expected ouput:
+
+//hari
+
+for (let student of arr) {
+  // console.log(name);
+  // let lowMarks=0;
+  for (let name in student) {
+    console.log(name);
+  }
+}
