ae6d20b828a79209cc71c94cb58ad01046865798
Ajay Gurung <gooronz.ajay@gmail.com>
2025-01-10 09:40:18 +0545

    alternative for level23 q2

--- level23.js
+++ level23.js
@@ -1,7 +1,7 @@
 
 //for in for of loop
 
-
+// Q1
 const obj = {
   math: 3,
   science:5,
@@ -22,7 +22,7 @@ for (score in obj){
 console.log(sum)
 
 
-
+// Q2
 const arr =[{ram:40},{gopal:50},{hari:60}]
 
 // using for of/for in output the highest marks scorer
@@ -34,7 +34,7 @@ let highest_scorer = ''
 
 // to find highest score. If at each iteration, new highest score is found, the highest_score gets updated
 for (item of arr){
-    for (person in item){    // to get the property name of current object
+    for (person in item){    // to get the property name of current object 
         if (item[person] > highest_score){  //comparing score of current person with current highest_sore
             highest_score = item[person]
             highest_scorer = person
@@ -44,3 +44,39 @@ for (item of arr){
 
 console.log(highest_score)
 console.log(highest_scorer)
+
+// Alternative: using only one loop
+highest_scorer = ''
+highest_score = 0
+for (let item of arr){
+    // covert {name:score} to [name, score]
+    // console.log(Object.entries(item))
+
+    const [name,score] = Object.entries(item)[0]
+
+    if(score > highest_score) {
+        highest_score = score
+        highest_scorer = name
+    }
+}
+
+console.log(highest_scorer)
+
+
+//Alternative: reduce loop
+
+const output = arr.reduce((highScorer, currentItem) => {
+    let [names, scores] = Object.entries(currentItem)[0]
+    if (scores > highScorer[0]){
+        highScorer[0] = scores
+        highScorer[1] = names
+    }
+    return highScorer;
+}, [0, '']);
+
+console.log(output[1])
+
+
+//using for loop, you have to solve problem within th same block of code
+// but within map, filter and reduce, we can return value or do optional chaining to create modules,
+// breaking down problem into different blocks
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,4 @@
-
-    console.log(counter[i])
+if (scores > highScorer[0]){
+        highScorer[0] = scores
+        highScorer[1] = names
+    }
