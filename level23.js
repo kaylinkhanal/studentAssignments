adfeb5b230e313ed8e6c0beb79e0f1a4ef3a29b9
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2025-01-13 09:07:36 +0545

    Nothing

--- level23.js
+++ level23.js
@@ -1,17 +1,24 @@
 const obj = {
   math: 3,
-  science:5,
-  comp: 4
-}
+  science: 5,
+  comp: 4,
+};
 
 //O O
-for (let item in obj){
-  console.log(item)
+let sum = 0;
+for (let item in obj) {
+  sum += obj[item];
 }
+
+console.log(sum);
 
 //Calculate the sum of all score
 
-const arr =[{ram:40},{gopal:50},{hari:60}]
+const arr = [{ ram: 40 }, { gopal: 50 }, { hari: 60 }];
+
+for (const item of arr) {
+  const [scores] = Object.values(item);
+}
 
 // let sum = 0
 // cart.forEach((item)=>{
@@ -19,13 +26,14 @@ const arr =[{ram:40},{gopal:50},{hari:60}]
 // })
 // console.log(sum)
 
+// for (const element of object) {
+//   console.log(element);
+// }
+
 //expected output: 11000
- const output  =cart.reduce((sum, nextItem,nextId)=>{
-   sum = sum + nextItem
-   return sum
-},0)
+// const output = cart.reduce((sum, nextItem, nextId) => {
+//   sum = sum + nextItem;
+//   return sum;
+// }, 0);
 
-
-console.log(output)
-
-
+// console.log(output);
