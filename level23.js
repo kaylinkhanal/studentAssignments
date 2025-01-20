4aca03cd82c641328a1ebc2c68ae1c020384a486
Kaylin Khanal <kyalinnop@gmail.com>
2025-01-20 08:36:39 +0545

    react setup

--- level23.js
+++ level23.js
@@ -4,15 +4,27 @@ const obj = {
   comp: 4
 }
 
+let sum = 0
 //O O
 for (let item in obj){
-  console.log(item)
+  sum = sum  + obj[item]
+  //do code changes
 }
-
+console.log(sum)
 //Calculate the sum of all score
 
 const arr =[{ram:40},{gopal:50},{hari:60}]
 
+const output = arr.reduce((highScorer, nextItem, nextId)=>{
+  const [name, scores] =  Object.entries(nextItem)[0]
+  if(scores> highScorer[0]){
+    highScorer[0]= scores
+    highScorer[1] = name
+  }
+return highScorer
+},[0,''])
+
+console.log(output)
 // using for of/for in output the highest marks scorer
 //expected ouput:
 //hari
