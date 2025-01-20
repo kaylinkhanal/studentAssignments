02c62f8b78eacab271c2a1dedef5c9eeff3d0840
Prem kala <114562215+Premkalagithub@users.noreply.github.com>
2025-01-20 08:38:57 +0545

    task add

---
+++ eventloop.js
@@ -0,0 +1,20 @@
+debugger;
+function child(){
+    debugger;
+     return 10
+}
+function parent(){
+    debugger;
+    return child() + 30
+}
+debugger;
+parent()
+debugger;
+
+//clousers;
+//javascript//
+//freecode camp//
+//leetcode//
+ //dsa// :algoexpert,educative
+//typescript///
+ 
