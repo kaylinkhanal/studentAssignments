b86dbb177fd5368529a0ea342c01e6477cacd1d4
Kaylin Khanal <kyalinnop@gmail.com>
2025-01-22 09:33:06 +0545

    add folder structure

---
+++ react-level1/src/app/(auth)/register/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+      <input/>
+      <input/>
+      <input/>
+      <input/>
+      <input/>
+      <input/>
+      <input/>
+      <input/>
+    </div>
+  )
+}
+
+export default Register
