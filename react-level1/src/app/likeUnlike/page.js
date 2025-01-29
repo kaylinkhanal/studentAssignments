2bd7988aca093fe47a4c6bdf511ef917e54de8e5
Samirghimire-5 <samirghimire9841@gmail.com>
2025-01-29 08:01:58 +0545

    code

--- react-level1/src/app/likeUnlike/page.js
+++ react-level1/src/app/likeUnlike/page.js
@@ -8,26 +8,28 @@ const likeUnlike = () => {
 
   const liked = () => {
     setLike(!like);
+    setDislike(false)
   };
 
   const disliked = () => {
-    setDislike(!dislike)
-  }
+    setDislike(!dislike);
+    setLike(false)
+  };
 
   return (
     <div className="flex gap-8 mx-10">
       <div className="flex">
-        <p>{like ? "liked" : ""}</p>
+        <p className="w-12">{like ? "liked" : ""}</p>
         <ThumbsUp
           onClick={liked}
           className={like ? "text-blue-700" : "text-gray-500"}
         />
       </div>
       <div className="flex">
-        <p>{dislike ? "dislike" : ""}</p>
+        <p className="w-12">{dislike ? "dislike" : ""}</p>
         <ThumbsDown
-        onClick={disliked}
-        className={dislike ? "text-blue-700" : "text-gray-500"}
+          onClick={disliked}
+          className={dislike ? "text-blue-700" : "text-gray-500"}
         />
       </div>
     </div>
