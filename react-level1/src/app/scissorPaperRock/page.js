7230b57b5544ee39b0f1dff15a80166b138f6844
Samirghimire-5 <samirghimire9841@gmail.com>
2025-02-04 15:56:52 +0545

    added Rock Paper Scissor game

---
+++ react-level1/src/app/scissorPaperRock/page.js
@@ -0,0 +1,76 @@
+"use client";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
+const ScissorPaperRock = () => {
+  const [playerMove, setplayerMove] = useState(null);
+  const [computerMove, setComputerMove] = useState(null);
+
+  const winner = () => {
+    if (!playerMove || !computerMove) return "Choose a move!";
+    if (playerMove === computerMove) return "Tie!";
+
+    if (
+      (playerMove === "paper" && computerMove === "rock") ||
+      (playerMove === "rock" && computerMove === "scissor") ||
+      (playerMove === "scissor" && computerMove === "paper")
+    ) {
+      return "You Win!!";
+    } else {
+      return "You Lose!!";
+    }
+  };
+
+  const playerSelect = (move) => {
+    setplayerMove(move);
+
+    let randomNum = Math.ceil(Math.random() * 3);
+    let computerSelect =
+      randomNum === 1 ? "rock" : randomNum === 2 ? "paper" : "scissor";
+    setComputerMove(computerSelect);
+  };
+
+  return (
+    <div className="flex bg-gray-900 flex-col items-center justify-center h-screen">
+      <>
+        <section className="flex gap-8">
+          <div
+            onClick={() => playerSelect("rock")}
+            className="m-2 p-2 border-4 border-blue-500 rounded-full bg-white"
+          >
+            <Image src={"/fist.png"} width={100} height={100} alt={"rock"} />
+          </div>
+
+          <div
+            onClick={() => playerSelect("paper")}
+            className="m-2 p-2 border-4 border-blue-500 rounded-full bg-white"
+          >
+            <Image src={"/hand.png"} width={100} height={100} alt={"rock"} />
+          </div>
+        </section>
+
+        <section>
+          <div
+            onClick={() => playerSelect("scissor")}
+            className="m-2 p-2 border-4 border-blue-500 rounded-full bg-white"
+          >
+            <Image src={"/scissor.png"} width={100} height={100} alt={"rock"} />
+          </div>
+        </section>
+
+        <section className="flex flex-col items-center justify-center gap-2 text-white h-[100] w-[530]">
+          <div className="flex justify-between gap-8 font-mono font-bold text-xl">
+            <p>Your Move: {playerMove}</p>
+            <p>Computer Move: {computerMove}</p>
+          </div>
+
+          <div className="font-bold font-mono text-3xl">
+            <p>{winner()}</p>
+          </div>
+        </section>
+      </>
+    </div>
+  );
+};
+
+export default ScissorPaperRock;
--- react-level1/src/app/unclick/page.js
+++ react-level1/src/app/unclick/page.js
@@ -1,9 +1,41 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
 
 const UnClick = () => {
-  return (
-    <div>UnClick</div>
-  )
-}
+  const [selectedItems, setSelectedItems] = useState("");
 
-export default UnClick
+  const grids = [
+    ["🚈", "✨", "🕔"],
+    ["🚍", "🚈", "🕔"],
+    ["✨", "👽", "🦏"],
+    ["🦏", "🚍", "👽"],
+  ];
+  const handleClick = (position) => {
+    setSelectedItems(position);
+  };
+
+  return (
+    <div>
+      {selectedItems}
+      {grids.map((item, id) => {
+        return (
+          <div className="flex" key={id}>
+            {item.map((emoji, idx) => {
+              return (
+                <div
+                  key={idx}
+                  onClick={() => handleClick(id + idx.toString())}
+                  className="p-2 m-2 shadow-md w-16 h-16 bg-violet-800 text-4xl rounded-lg"
+                >
+                  {id + idx.toString() === selectedItems && emoji}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default UnClick;
