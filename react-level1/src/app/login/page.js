8d76219c46cd3bd4b397ee902c415692f48d5820
Bibek Bhatta <bhattabbk1@gmail.com>
2025-02-09 08:36:39 +0545

    -todos

---
+++ react-level1/src/app/login/page.js
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>Login Page
+     <Button>Click Me</Button> 
+    </div>
+  )
+}
+
+export default Login
---
+++ react-level1/src/components/SideNav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SideNav = () => {
+  const NavItems = ['Status','Communities','Channel','Chat']
+  return (
+    <div>
+
+
+
+
+    </div>
+  )
+}
+
+export default SideNav
